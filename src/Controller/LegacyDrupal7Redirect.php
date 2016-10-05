0a65d3b7408460491589ecffe1777624ee70f49f
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-10-05 21:54:44 +0200

    WIP.

---
+++ src/Controller/LegacyDrupal7Redirect.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\language_selection_page\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Url;
+
+/**
+ * Controller for legacy route (Drupal 7 path to Drupal 8 path).
+ */
+class LegacyDrupal7Redirect extends ControllerBase {
+
+  /**
+   * Callback for the language_selection_page.negotiation_language_selection_page_legacy_redirect route.
+   */
+  public function doRedirect() {
+    return new RedirectResponse(Url::fromRoute('language_selection_page.negotiation_selection_page')->setAbsolute()->toString());
+  }
+
+}
