6d2696ce12d1947d7ea3bd8d1f5919a5103ffa1e
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-10-07 03:26:38 +0200

    WIP.

--- src/Plugin/LanguageSelectionPageCondition/LanguageSelectionPageConditionIgnoreNeutral.php
+++ src/Plugin/LanguageSelectionPageCondition/LanguageSelectionPageConditionIgnoreNeutral.php
@@ -2,8 +2,9 @@
 
 namespace Drupal\language_selection_page\Plugin\LanguageSelectionPageCondition;
 
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\language_selection_page\LanguageSelectionPageConditionBase;
 use Drupal\language_selection_page\LanguageSelectionPageConditionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -21,13 +22,39 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LanguageSelectionPageConditionIgnoreNeutral extends LanguageSelectionPageConditionBase implements LanguageSelectionPageConditionInterface {
 
   /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * LanguageSelectionPageConditionIgnoreNeutral constructor.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The Route Match object
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(RouteMatchInterface $route_match, array $configuration, $plugin_id, array $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
+      $container->get('current_route_match'),
       $configuration,
       $plugin_id,
-      $plugin_definition);
+      $plugin_definition
+    );
   }
 
   /**
@@ -35,10 +62,13 @@ class LanguageSelectionPageConditionIgnoreNeutral extends LanguageSelectionPageC
    */
   public function evaluate() {
     if ($this->getConfiguration()[$this->getPluginId()] == TRUE) {
-      /** @var EntityInterface $entity */
-      $entity = $this->configuration['request']->attributes->get('node');
-      if (in_array($entity->language()->getId(), ['und', 'zxx'])) {
-        $this->block();
+
+      foreach ($this->routeMatch->getParameters() as $parameter) {
+        if ($parameter instanceof ContentEntityInterface) {
+          if (!$parameter->isTranslatable()) {
+            return $this->block();
+          }
+        }
       }
     }
 
@@ -50,10 +80,10 @@ class LanguageSelectionPageConditionIgnoreNeutral extends LanguageSelectionPageC
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form[$this->getPluginId()] = [
-      '#title' => $this->t('Ignore language neutral entities and content types.'),
+      '#title' => $this->t('Ignore untranslatable (language neutral) entities.'),
       '#type' => 'checkbox',
       '#default_value' => $this->configuration[$this->getPluginId()],
-      '#description' => $this->t('Do not redirect to the language selection page if the entity is language neutral or if the content do not have multilingual support.'),
+      '#description' => $this->t('Do not redirect to the language selection page if the entity on the page being viewed is not translatable (such as when it is language neutral, or if the content type it belongs to is not translatable).'),
     ];
 
     return $form;
